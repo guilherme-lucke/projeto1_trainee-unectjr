efee91eedf90d0bd4ccc5b80066bb400b0f1d568
Guilherme Lucke <luckegg@outlook.com>
2023-05-04 12:36:50 -0300

    Api's update

--- js/api's.js
+++ js/api's.js
@@ -1,19 +1,25 @@
 const adviceElement = document.querySelector('.frase p');
 
-fetch('https://api.adviceslip.com/advice')
-    .then(response => response.json())
-    .then(data => {
-        const advice = data.slip.advice; 
+function getAdvice() {
+    fetch('https://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then(data => {
+            const advice = data.slip.advice;
 
-        const apiUrl = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(advice)}&langpair=en|pt-BR`;
-        return fetch(apiUrl);
-    })
-    .then(response => response.json())
-    .then(data => {
-        const translatedAdvice = data.responseData.translatedText;
-        adviceElement.textContent = translatedAdvice;
-    })
-    .catch(error => {
-        console.error('Ocorreu um erro:', error);
-        adviceElement.textContent = 'Não foi possível carregar a frase do dia.';
-    });
+            const apiUrl = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(advice)}&langpair=en|pt-BR`;
+            return fetch(apiUrl);
+        })
+        .then(response => response.json())
+        .then(data => {
+            const translatedAdvice = data.responseData.translatedText;
+            adviceElement.textContent = translatedAdvice;
+        })
+        .catch(error => {
+            console.error('Ocorreu um erro:', error);
+            adviceElement.textContent = 'Não foi possível carregar a frase do dia.';
+        });
+}
+
+getAdvice();
+
+setInterval(getAdvice, 24 * 60 * 60 * 1000);
