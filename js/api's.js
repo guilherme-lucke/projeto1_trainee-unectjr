a07a8c1f7dd87d4edc332034b5c25b4cce21b399
Guilherme Lucke <luckegg@outlook.com>
2023-05-04 17:30:52 -0300

    Api's update

--- js/api's.js
+++ js/api's.js
@@ -1,6 +1,14 @@
 const adviceElement = document.querySelector('.frase p');
 
 function getAdvice() {
+    const lastRequestDate = localStorage.getItem('last_request_date');
+    const today = new Date().toISOString().slice(0, 10);
+
+    if (lastRequestDate === today) {
+        // Não faz a requisição, pois já foi feita hoje
+        return;
+    }
+
     fetch('https://api.adviceslip.com/advice')
         .then(response => response.json())
         .then(data => {
@@ -13,6 +21,9 @@ function getAdvice() {
         .then(data => {
             const translatedAdvice = data.responseData.translatedText;
             adviceElement.textContent = translatedAdvice;
+
+            // Atualiza a data da última requisição no localStorage
+            localStorage.setItem('last_request_date', today);
         })
         .catch(error => {
             console.error('Ocorreu um erro:', error);
